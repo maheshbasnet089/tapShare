311b7337b7c805c0b5a1795be731b030cf883fca
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-06 10:49:17 +0545

    add baseUrl

---
+++ tapShareFrontend/src/config/index.js
@@ -0,0 +1,8 @@
+// for production use
+module.exports = {
+  baseUrl: "https://tapshare.onrender.com/api/v1",
+};
+// for development use
+// module.exports = {
+//   baseUrl: "http://localhost:1337/api/v1",
+// };
--- tapShareFrontend/src/store.js
+++ tapShareFrontend/src/store.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
+import { baseUrl } from "./config";
 
 export const useStore = create((set) => ({
   send_file: async (file, email) => {
@@ -10,15 +11,11 @@ export const useStore = create((set) => ({
       formData.append("files", file[i]);
     }
     console.log(formData.get("files"));
-    const res = await axios.post(
-      "http://localhost:1337/api/v1/sendFile",
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    const res = await axios.post(`${baseUrl}/sendFile`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
     console.log(res.data);
   },
 }));
