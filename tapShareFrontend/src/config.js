f16723cc7459b804feffac857ad337ca432748f5
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-06 10:52:27 +0545

    add baseUrl

---
+++ tapShareFrontend/src/config.js
@@ -0,0 +1,5 @@
+export const baseUrl = "https://tapshare.onrender.com/api/v1";
+
+//for development use uncomment the following line
+
+// export const baseUrl = "http://localhost:1337/api/v1";
--- tapShareFrontend/src/config/index.js
+++
@@ -1,8 +0,0 @@
-// for production use
-module.exports = {
-  baseUrl: "https://tapshare.onrender.com/api/v1",
-};
-// for development use
-// module.exports = {
-//   baseUrl: "http://localhost:1337/api/v1",
-// };
