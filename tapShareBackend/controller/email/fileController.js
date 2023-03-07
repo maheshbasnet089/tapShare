e311a892d9e1bcf028d2f59734b4de90573f9b79
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-07 09:21:18 +0545

    add trycatch

--- tapShareBackend/controller/email/fileController.js
+++ tapShareBackend/controller/email/fileController.js
@@ -4,54 +4,62 @@ const sendSms = require("../../services/sendSms");
 
 exports.sendFiles = async (req, res) => {
   const files = req.files;
+  try {
+    const filePaths = [];
 
-  const filePaths = [];
-
-  for (var i = 0; i < files.length; i++) {
-    console.log("path", files[i].path);
-    const newFile = await File.create({
-      name: files[i].originalname,
-      path: files[i].path.replace(/\\/g, "/").replace("uploads/", ""), // replace backslash with forward slash
-      size: files[i].size,
-    });
-    console.log(newFile);
-
-    const savedFile = await newFile.save();
-    if (savedFile) filePaths.push(newFile.path);
-  }
-  // send email here
-  const emailOptions = {
-    to: req.body.email,
-    subject: "New files received",
-    text: "New files received",
-  };
-
-  // Add file paths as links in the email body
-  emailOptions.text += "\nFile paths:\n";
-  for (const filePath of filePaths) {
-    emailOptions.text += `${process.env.baseUrl}${filePath}\n`;
-  }
-
-  if (req.body.email.startsWith("98")) {
-    console.log(req.body.email);
-    try {
-      await sendSms(emailOptions);
-      return res.json({
-        message: "File sent successfully",
-        status: 200,
+    for (var i = 0; i < files.length; i++) {
+      console.log("path", files[i].path);
+      const newFile = await File.create({
+        name: files[i].originalname,
+        path: files[i].path.replace(/\\/g, "/").replace("uploads/", ""), // replace backslash with forward slash
+        size: files[i].size,
       });
-    } catch (e) {
-      return res.json({
-        message: "Error sending sms",
-        status: 500,
-      });
+      console.log(newFile);
+
+      const savedFile = await newFile.save();
+      if (savedFile) filePaths.push(newFile.path);
     }
-  } else {
-    sendEmail(emailOptions);
-  }
+    // send email here
+    const emailOptions = {
+      to: req.body.email,
+      subject: "New files received",
+      text: "New files received",
+    };
 
-  res.json({
-    message: "File sent successfully",
-    status: 200,
-  });
+    // Add file paths as links in the email body
+    emailOptions.text += "\nFile paths:\n";
+    for (const filePath of filePaths) {
+      emailOptions.text += `${process.env.baseUrl}${filePath}\n`;
+    }
+
+    if (req.body.email.startsWith("98")) {
+      console.log(req.body.email);
+      try {
+        await sendSms(emailOptions);
+        return res.json({
+          message: "File sent successfully",
+          status: 200,
+        });
+      } catch (e) {
+        return res.json({
+          message: "Error sending sms",
+          status: 500,
+        });
+      }
+    } else {
+      sendEmail(emailOptions);
+    }
+
+    res.json({
+      message: "File sent successfully",
+      status: 200,
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      errorMessage: e.message,
+      message: "Error sending file",
+      status: 500,
+    });
+  }
 };
