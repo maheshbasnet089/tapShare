43b393ccdb8d41d4e632df3657322ef0bf720cf8
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-08 16:57:27 +0545

    fix url

--- tapShareFrontend/src/main.jsx
+++ tapShareFrontend/src/main.jsx
@@ -1,20 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { StyledEngineProvider } from "@mui/material/styles";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import App from "./App";
+// import "./index.css";
+// import SeeFiles from "./SeeFiles";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     {/* <StyledEngineProvider injectFirst> */}
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />} />
+//         <Route path="/a/:id" element={<SeeFiles />} />
+//       </Routes>
+//     </BrowserRouter>
+//     {/* </StyledEngineProvider> */}
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StyledEngineProvider } from "@mui/material/styles";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import SeeFiles from "./SeeFiles";
+<script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <StyledEngineProvider injectFirst> */}
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/a/:id" element={<SeeFiles />} />
-      </Routes>
-    </BrowserRouter>
-    {/* </StyledEngineProvider> */}
+    <App />
   </React.StrictMode>
 );
