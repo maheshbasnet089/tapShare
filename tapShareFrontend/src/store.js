4f2e2093824a7b6c843878b4ad83efe2a4ee2136
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-08 16:13:45 +0545

    change url

--- tapShareFrontend/src/store.js
+++ tapShareFrontend/src/store.js
@@ -29,8 +29,8 @@ export const useStore = create((set) => ({
     }
 
     const res = await axios.post(
-      "http://localhost:1337/api/v1/sendFile",
-      // "https://tapshare.onrender.com/api/v1/sendFile",
+      // "http://localhost:1337/api/v1/sendFile",
+      "https://tapshare.onrender.com/api/v1/sendFile",
       formData,
       {
         headers: {
@@ -42,11 +42,10 @@ export const useStore = create((set) => ({
       alert(res.data.message);
     } else if (res.data.status === 201) {
       window.location.href =
-        "http://127.0.0.1:5173/" + localStorage.getItem("userId");
+        "https://tapshare.xyz/" + localStorage.getItem("userId");
       // navigate("/seeAllMyFiles");
     } else {
       alert("Error sending file");
     }
-
   },
 }));
