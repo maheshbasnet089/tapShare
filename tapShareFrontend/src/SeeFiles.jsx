e97dbf51fe937f9370e4c062bb2a9a954fb22807
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-08 16:23:56 +0545

    change url

--- tapShareFrontend/src/SeeFiles.jsx
+++ tapShareFrontend/src/SeeFiles.jsx
@@ -5,13 +5,12 @@ import { useParams } from "react-router-dom";
 const SeeFiles = () => {
   const { id } = useParams();
   const [files, setFiles] = React.useState([]); // [state, setState]
- 
+
   const fetchFiles = async function fetchFiles() {
-    const res = await axios.get(`http://localhost:1337/${id}`);
-    
+    const res = await axios.get(`https://tapshare.onrender.com/${id}`);
+
     if (res.data.status === 200) {
       setFiles(res.data.files);
-      
     } else {
       alert(res.data.message);
     }
@@ -20,8 +19,15 @@ const SeeFiles = () => {
     fetchFiles();
   }, []);
   return (
-    <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center"}}>
-      <h1 style={{marginBottom:"40px"}}>Shared Files (tap to download)</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ marginBottom: "40px" }}>Shared Files (tap to download)</h1>
       {files.map((file) => {
         return (
           <h3 key={file._id}>
