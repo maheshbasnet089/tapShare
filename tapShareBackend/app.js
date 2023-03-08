dea1be0177238a006becd5a20a84a83e7f395ea4
maheshbasnet089 <basnetmanish089@gmail.com>
2023-03-08 20:53:55 +0545

    fix file not defined

--- tapShareBackend/app.js
+++ tapShareBackend/app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
+const File = require("./model/fileModel");
 
 //CORS
 const corsOptions = {
