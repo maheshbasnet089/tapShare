9c50b013aab8ff09c9b3b087e0d8fd1f91f92939
A18yan <ma18yan@gmail.com>
2023-03-19 00:10:59 +0545

    commit

--- tapShareFrontend/src/components/toaster.jsx
+++ tapShareFrontend/src/components/toaster.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
 import Slide from "@mui/material/Slide";
 import MuiAlert from "@mui/material/Alert";
