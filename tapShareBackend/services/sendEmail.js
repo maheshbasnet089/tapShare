841ada96322e0579be96b720e4caf4857d4ddb0b
maheshbasnet089 <basnetmanish089@gmail.com>
2023-05-23 14:02:53 +0545

    fix Email Err

--- tapShareBackend/services/sendEmail.js
+++ tapShareBackend/services/sendEmail.js
@@ -1,32 +1,35 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
- 
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    // host: "smtp.gmail.com",
-    // port: 465,
+  try {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      // host: "smtp.gmail.com",
+      // port: 465,
 
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
-  const mailOptions = {
-    from: "tapshare<tapshare@gmail.com>",
-    to: options.to,
-    subject: options.subject,
-    text: options.text,
-  };
-
-  transporter
-    .sendMail(mailOptions)
-    .then((info) => {
-      console.log("Email sent: " + info.response);
-    })
-    .catch((err) => {
-      console.log(err);
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
     });
+    const mailOptions = {
+      from: "tapshare<tapshare@gmail.com>",
+      to: options.to,
+      subject: options.subject,
+      text: options.text,
+    };
+
+    transporter
+      .sendMail(mailOptions)
+      .then((info) => {
+        console.log("Email sent: " + info.response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = sendEmail;
