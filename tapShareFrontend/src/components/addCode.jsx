f741966938221c4db5e888f3a643dc2295bf93c8
maheshbasnet089 <basnetmanish089@gmail.com>
2023-06-09 12:53:01 +0545

    fix route

--- tapShareFrontend/src/components/addCode.jsx
+++ tapShareFrontend/src/components/addCode.jsx
@@ -2,20 +2,22 @@ import { TextField } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import { baseUrl } from "../config";
+import { useNavigate } from "react-router-dom";
 
 const AddCode = () => {
-  // function generateUserId() {
-  //   const userId = Math.floor(1000 + Math.random() * 9000);
-  //   return userId;
-  // }
-  // if (
-  //   localStorage.getItem("userId") == null ||
-  //   localStorage.getItem("userId") == "" ||
-  //   localStorage.getItem("userId") == undefined
-  // ) {
-  //   const userId = generateUserId();
-  //   localStorage.setItem("userId", userId);
-  // }
+  const navigate = useNavigate();
+  function generateUserId() {
+    const userId = Math.floor(1000 + Math.random() * 9000);
+    return userId;
+  }
+  if (
+    localStorage.getItem("userId") == null ||
+    localStorage.getItem("userId") == "" ||
+    localStorage.getItem("userId") == undefined
+  ) {
+    const userId = generateUserId();
+    localStorage.setItem("userId", userId);
+  }
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -23,6 +25,9 @@ const AddCode = () => {
     formData.append("userId", "f" + localStorage.getItem("userId"));
     const data = Object.fromEntries(formData);
     const response = await axios.post(`${baseUrl}api/v1/code`, data);
+    if (response.data.status == 200) {
+      navigate("/" + response.data.code.userId);
+    }
     console.log(response.data);
     console.log(data);
   };
