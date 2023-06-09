265c0fd3764afb07ec0dfee2da83abd3252778a9
maheshbasnet089 <basnetmanish089@gmail.com>
2023-06-09 12:56:31 +0545

    fix status

--- tapShareBackend/controller/Code/codeController.js
+++ tapShareBackend/controller/Code/codeController.js
@@ -2,8 +2,8 @@ const Code = require("../../model/textModel");
 
 exports.createCode = async (req, res) => {
   try {
-    const { text, userId,title } = req.body;
-  
+    const { text, userId, title } = req.body;
+
     const code = await Code.create({
       text,
       userId,
@@ -11,11 +11,13 @@ exports.createCode = async (req, res) => {
     });
     if (code) {
       res.status(201).json({
+        status: 200,
         message: "Code created successfully",
         code: code,
       });
     } else {
       res.status(500).json({
+        status: 500,
         message: "Something went wrong",
       });
     }
@@ -30,7 +32,7 @@ exports.createCode = async (req, res) => {
 exports.getCode = async (req, res) => {
   try {
     const codeFound = await Code.find({ userId: req.params.id });
-  
+
     if (codeFound.length == 0) {
       return res.json({
         status: 404,
