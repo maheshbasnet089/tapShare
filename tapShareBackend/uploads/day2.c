11e1ed077cc29fe4390e3c9081b3a3e375a8893d
maheshbasnet089 <basnetmanish089@gmail.com>
2023-06-14 22:15:24 +0545

    add code

---
+++ tapShareBackend/uploads/day2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+
+    struct student
+    {
+           ingt id;
+           char name[50];
+           int roll;char phone[10];
+           char grade[20];
+           
+           };
+           struct student list [MAX];
+           int counter =0;
+           void add structure(){
+                print f ("welcome to student registration portal:");
+                printf("\nEnter your id:");
+                scanf("
+                
