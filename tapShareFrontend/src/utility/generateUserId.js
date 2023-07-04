83a59e3f31a436b6207f6e7663de1a3991862216
arpan404 <itsmearpan1@gmail.com>
2023-07-04 12:01:33 +0545

    made userid more simpler

--- tapShareFrontend/src/utility/generateUserId.js
+++ tapShareFrontend/src/utility/generateUserId.js
@@ -1,13 +1,13 @@
 export default function generateUserId() {
   //set epoch timestamp + some random text to prevent duplicate userId
-  let userId = parseInt(Date.now()).toString().slice(2);
+  let userId = Math.round(parseInt(Date.now()) / 1000)
+    .toString()
+    .slice(4);
   let randomString = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
-  for (let i = 0; i <= 5; i++) {
-    userId =
-      randomString[(Math.random() * (randomString.length - 1)).toFixed(0)] +
-      userId;
+  for (let i = 0; i < 2; i++) {
     userId +=
       randomString[(Math.random() * (randomString.length - 1)).toFixed(0)];
   }
   return userId;
 }
+console.log(generateUserId());
