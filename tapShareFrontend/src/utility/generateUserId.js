aceb8da6c791a33b8184e5aaa11d2a6faa35c514
maheshbasnet089 <basnetmanish089@gmail.com>
2023-07-05 22:12:33 +0545

    fix userId

--- tapShareFrontend/src/utility/generateUserId.js
+++ tapShareFrontend/src/utility/generateUserId.js
@@ -1,13 +1,11 @@
 export default function generateUserId() {
   //set epoch timestamp + some random text to prevent duplicate userId
-  let userId = Math.round(parseInt(Date.now()) / 1000)
-    .toString()
-    .slice(4);
-  let randomString = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
-  for (let i = 0; i < 2; i++) {
-    userId +=
-      randomString[(Math.random() * (randomString.length - 1)).toFixed(0)];
-  }
+  const userId = Math.floor(100000 + Math.random() * 900000);
+
+  // let randomString = "qwertyuiopasdfghjklzxcvbnm";
+  // for (let i = 0; i < 2; i++) {
+  //   userId +=
+  //     randomString[(Math.random() * (randomString.length - 1)).toFixed(0)];
+  // }
   return userId;
 }
-
--- tapShareFrontend/src/utility/store.js
+++ tapShareFrontend/src/utility/store.js
@@ -20,6 +20,7 @@ export const useStore = create((set) => ({
       set({ fireButton: true });
     }
     const formData = new FormData();
+    console.log(email);
     formData.append("email", email);
     formData.append("userId", localStorage.getItem("userId"));
     for (let i = 0; i < file.length; i++) {
@@ -52,8 +53,8 @@ export const useStore = create((set) => ({
         setFiles(null);
       } else if (res.data.status === 201) {
         window.location.href =
-          "https://tapshare.xyz/" + localStorage.getItem("userId");
-        // "http://127.0.0.1:5173/" + localStorage.getItem("userId");
+          // "https://tapshare.xyz/" + localStorage.getItem("userId");
+          "http://127.0.0.1:5173/" + localStorage.getItem("userId");
         // navigate("/seeAllMyFiles");
       } else {
         setToasterData({
