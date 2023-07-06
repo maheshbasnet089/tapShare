f64f8f83e49d19f8c2c2561d5c0ed1b8bf2f0ace
arpan404 <itsmearpan1@gmail.com>
2023-07-06 10:28:35 +0545

    Redesigned the generate link button

--- tapShareFrontend/src/components/generateLink.jsx
+++ tapShareFrontend/src/components/generateLink.jsx
@@ -7,28 +7,24 @@ const GenerateLink = () => {
   const files = useStore((state) => state.files);
   const loading = useStore((state) => state.loading);
   return (
-    <div
-      className={`flex ${
-        !loading && "pl-[.8em] pr-[.5em]"
-      } w-fit mt-[.5em] bg-[lightgray]  rounded shadow-md shadow-[#555] cursor-pointer`}
-    >
-        {loading ? (
-          <img
-            src={Loader}
-           
-            srcSet=""
-            className="h-[2.25em] w-[6em] object-cover"
-          />
-        ) : (
-          <h2
-            className="hover:text-[#777676]"
-            onClick={() => send_file(files, "")}
-            title="generate link"
-          >
-            Generate Link
-          </h2>
-        )}
-    </div>
+    <>
+      {loading ? (
+        <img
+          src={Loader}
+          srcSet=""
+          className="h-[2.25em] w-[6em] object-cover"
+        />
+      ) : (
+        <button
+          role="button"
+          className="bg-blue-500 p-0 text-gray-50 rounded-full text-center mt-4 font-semibold hover:bg-blue-600 ease-in transition-all duration-300 hover:scale-110"
+          onClick={() => send_file(files, "")}
+          title="Generate Link"
+        >
+          Generate Link
+        </button>
+      )}
+    </>
   );
 };
 
