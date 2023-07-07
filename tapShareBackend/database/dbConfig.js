9c28d5c174de2e26d6eca950a3db2a004b470e12
ngr-np <ngr112001@gmail.com>
2023-07-08 00:09:41 +0545

    enhances the database connection code to improve logging and configuration.

--- tapShareBackend/database/dbConfig.js
+++ tapShareBackend/database/dbConfig.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
+const { MONGO_URI } = require("../config/secrets");
 
-// Database connection
-function mongoConnection(url) {
-  mongoose
-    .connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Database connected");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+const mongoConnection = () => {
+  try {
+    mongoose.connect(MONGO_URI);
+    console.log("waiting DB to connect");
+  } catch (err) {
+    console.log(err);
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected Successfully!!");
+});
 
 module.exports = mongoConnection;
