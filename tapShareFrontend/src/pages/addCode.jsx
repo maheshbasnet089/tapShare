e66ec35e26467a4c9c4bcc25a153d9a418b403c6
arpan404 <itsmearpan1@gmail.com>
2023-07-07 14:19:02 +0545

    Redesigned page for sending code

--- tapShareFrontend/src/pages/addCode.jsx
+++ tapShareFrontend/src/pages/addCode.jsx
@@ -1,28 +1,19 @@
-import { Box, Button, Grid, TextField, TextareaAutosize } from "@mui/material";
 import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { baseUrl } from "../config";
 import { useNavigate } from "react-router-dom";
-import AddIcon from "@mui/icons-material/Add";
-import { styled, useTheme } from "@mui/material/styles";
 import "../styles/addCode.css";
 import { useStore } from "../utility/store";
 import UploadingAnimation from "../components/animated/uploadingAnimation";
 import storeUser from "../utility/storeUser";
-
-const CodeWrap = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#02162a" : "#02162a",
-  ...theme.typography.body2,
-
-  textAlign: "center",
-  color: "white",
-  padding: "15px",
-}));
-
+import { VscNewFile } from "react-icons/vsc";
+import { RxCross2 } from "react-icons/rx";
 const AddCode = () => {
   const loading = useStore((state) => state.loading);
   const setLoading = useStore((state) => state.setLoading);
   const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
@@ -40,151 +31,93 @@ const AddCode = () => {
       setLoading(false);
     }
   };
-  const theme = useTheme();
-  const inputRef = useRef(null);
+  const clearData = ()=>{
+    setText("");
+    setTitle("");
+  }
+  const handleCancel = ()=>{
+    setLoading(false);
+    clearData();
+    navigate("/");
+  }
+  const handleNewCode = ()=>{
+    if(!loading){
+      clearData();
+    }
+  }
+
   useEffect(() => {
     {
       /** This function creates and store user Id, if it doesnot exist */
     }
     storeUser();
-  });
+  }, []);
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Box sx={{ width: "95%", pt: 4 }}>
-        <form onSubmit={handleSubmit}>
-          <CodeWrap borderRadius={"5px 5px 0 0"} sx={{ paddingTop: "20px" }}>
-            <Box sx={{ display: "flex", gap: 2 }}>
-              <Button
-                variant="outlined"
-                size="small"
-                onClick={() => window.location.reload()}
-                sx={{
-                  color: "white",
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "3px",
-                  ml: 1,
-                }}
-              >
-                <AddIcon />
-                New Code
-              </Button>
-              <TextField
-                id="outlined-basic"
-                name="title"
+    <>
+      <div className="px-2 py-2">
+        <div className="flex justify-end pt-2">
+          <button
+            className="flex bg-gray-200 gap-2 items-center rounded-xl uppercase font-bold hover:bg-white transition-all ease-in-out duration-100 h-10 w-[170px] text-slate-700 hover:text-slate-800 active:bg-gray-300"
+            role="button"
+            title="Share new text"
+            onClick={handleNewCode}
+          >
+            <VscNewFile className="text-2xl" /> New Code
+          </button>
+        </div>
+        <div>
+          <form
+            onSubmit={handleSubmit}
+            className="bg-[#22283C] mt-10 rounded-xl px-2"
+          >
+            <div className="px-2 pt-6 flex justify-between flex-wrap-reverse gap-2 items-center">
+              <input
+                type="text"
                 placeholder="Enter a title"
-                variant="outlined"
+                name="title"
+                className="h-12 w-full max-w-[300px] rounded-lg px-3 bg-[#303A55] focus:bg-[#252d41] outline-none text-gray-100 transition-all ease-in-out duration-200 focus:outline-2 focus:outline-blue-800"
                 required
-                InputProps={{
-                  style: {
-                    color: "white",
-                  },
-                }}
-                InputLabelProps={{
-                  style: {
-                    color: "white",
-                  },
-                }}
-                sx={{
-                  color: "white",
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderColor: "white",
-                    },
-                    "&:hover fieldset": {
-                      borderColor: "white",
-                    },
-                  },
-                  "& .MuiInputLabel-root": {
-                    "&:hover": {
-                      color: "white",
-                    },
-                  },
-                }}
-                size="small"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
-            </Box>
-          </CodeWrap>
-
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Box
-                sx={{
-                  px: 2,
-                  pt: 2,
-                  pb: 3,
-                  backgroundColor:
-                    theme.palette.mode === "dark" ? "#02162a" : "#02162a",
-                  ...theme.typography.body2,
-                  padding: theme.spacing(1),
-                  textAlign: "center",
-                  color: "white",
-                }}
+              <div
+                className="flex justify-center bg-red-500 gap-2 items-center rounded-xl uppercase font-bold hover:bg-red-600 transition-all ease-in-out duration-100 h-10 w-[130px] text-gray-100 hover:text-gray-50 active:bg-red-300"
+                role="button"
+                onClick={handleCancel}
               >
-                <Grid container spacing={1} sx={{ pb: 0 }}>
-                  <Grid item xs={10}>
-                    <TextareaAutosize
-                      aria-label="textarea"
-                      minRows={18}
-                      name="text"
-                      required
-                      placeholder="Enter or paste your text/code here.."
-                      style={{
-                        width: "100%",
-                        padding: "15px 10px",
-                        backgroundColor: "transparent",
-                        border: "1px solid gray",
-                        maxHeight: "53vh",
-                        overflow: "scroll",
-                        overflowX: "hidden",
-                      }}
-                    />
-                  </Grid>
-                  <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    sx={{
-                      display: "flex",
-                      justifyContent: "flex-start",
-                      gap: 1,
-                    }}
+                Cancel <RxCross2 className="text-2xl" />
+              </div>
+            </div>
+            <div className="p-2 mt-1 w-full">
+              <textarea
+                name="text"
+                className="w-full max-h-[calc(100dvh-400px)] min-h-[400px] rounded-lg px-3 bg-[#303A55] focus:bg-[#252d41] outline-none text-gray-100 transition-all ease-in-out duration-200 focus:outline-2 focus:outline-blue-800 py-3"
+                required
+                placeholder="Enter or paste your text/code here.."
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                title="Enter the text to share"
+              ></textarea>
+            </div>
+            <div className="h-[100px] px-2 flex items-center">
+              {loading ? (
+                <UploadingAnimation />
+              ) : (
+                <>
+                  <button
+                    role="submit"
+                    className="bg-blue-500 p-0 text-gray-50 rounded-full text-center font-semibold hover:bg-blue-600 ease-in transition-all duration-300 hover:scale-110 cursor-pointer"
+                    title="Share this text"
                   >
-                    <div className="h-[100px] px-2 flex items-center">
-                      {loading ? (
-                        <UploadingAnimation />
-                      ) : (
-                        <>
-                          <button
-                            role="submit"
-                            className="bg-blue-500 p-0 text-gray-50 rounded-full text-center font-semibold hover:bg-blue-600 ease-in transition-all duration-300 hover:scale-110 cursor-pointer"
-                            title="Share this text"
-                          >
-                            Share Now
-                          </button>
-                        </>
-                      )}
-                    </div>
-                  </Grid>
-                  <Grid
-                    item
-                    xs={12}
-                    md={5}
-                    className="css-form-control"
-                    sx={{
-                      padding: "0",
-                      display: "flex",
-                      alignItems: "flex-end",
-                    }}
-                  ></Grid>
-                </Grid>
-              </Box>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Box>
+                    Share Now
+                  </button>
+                </>
+              )}
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
