df0bd3c1220e37c4528108ddf970ea61078f39ea
arpan404 <itsmearpan1@gmail.com>
2023-07-25 02:19:05 +0545

    made code dynamic

--- tapShareFrontend/src/components/misc/TopBadge.jsx
+++ tapShareFrontend/src/components/misc/TopBadge.jsx
@@ -1,10 +1,12 @@
+import { useParams } from "react-router-dom";
 export default function TopBadge() {
+  const { id } = useParams();
   return (
     <div className="flex justify-between bg-gray-50 py-4 rounded-md px-3 border-l-green-500 border-l-8">
       <b className="select-none">Expires After : 24 hrs</b>
       <div className="flex">
         <b className="font-semibold select-none">Code</b> {" : "}{" "}
-        <h2 className="text-blue-500 font-medium select-text">213456</h2>
+        <h2 className="text-blue-500 font-medium select-text">{id}</h2>
       </div>
     </div>
   );
