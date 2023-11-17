849d1eff0713ab0ddcb2b543309c0f9fcdba45a6
NGR-NP <ngr112001@gmail.com>
2023-11-17 20:26:29 +0545

    refactor: remove unused dynamic imports for CodeDetails and FilesDetails

--- tapShareFrontend/src/App.jsx
+++ tapShareFrontend/src/App.jsx
@@ -2,12 +2,7 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register";
 import LoadingScreen from "./components/animated/LoadingScreen";
-const FilesDetails = React.lazy(() =>
-  import("./pages/history/components/files/files-details")
-);
-const CodeDetails = React.lazy(() =>
-  import("./pages/history/components/codes/code-details")
-);
+
 const Home = React.lazy(() => import("./pages/Home"));
 const AddCode = React.lazy(() => import("./pages/addCode"));
 const ViewCode = React.lazy(() => import("./pages/viewCode"));
@@ -26,8 +21,6 @@ const App = () => {
           <Route path="/code/:id" element={<ViewCode />} />
           <Route path="/:id" element={<SeeShared />} />
           <Route path="/history" element={<History />} />
-          <Route path="/history/code/:id" element={<CodeDetails />} />
-          <Route path="/history/file/:id" element={<FilesDetails />} />
           <Route path="*" element={<AreYouLost />} />
         </Routes>
       </Suspense>
