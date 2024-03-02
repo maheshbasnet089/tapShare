31d69dea91c5151aaac70085b1db215ecb9fd6e0
NGR-NP <ngr112001@gmail.com>
2024-03-03 02:06:08 +0545

    cc

--- tapShareBackend/app.js
+++ tapShareBackend/app.js
@@ -15,6 +15,7 @@ const corsOptions = {
     "https://tapshare.xyz",
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://bucolic-piroshki-04f0ec.netlify.app"
   ],
   // origin: "http://127.0.0.1:5173",
   // credentials: true, //access-control-allow-credentials:true
