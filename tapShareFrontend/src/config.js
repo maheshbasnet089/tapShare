836d88669cbd81e846dc10540693f457c188c054
NGR-NP <ngr112001@gmail.com>
2024-03-07 21:16:26 +0545

    feat: insted of hardcoing urls getting it form env

--- tapShareFrontend/src/config.js
+++ tapShareFrontend/src/config.js
@@ -1,11 +1,14 @@
-// export const baseUrl = "https://tapshare.dpkailashcollab.com/";
-export const baseUrl = "https://tapshare.digitalpathshalanepal.com/";
+/**
+ * Configuration for frontend app URLs.
+ * Loaded from .env file. Variable names should start with VITE_.
+ * @type {string}
+ */
+export const baseUrl = import.meta.env.VITE_BASE_URL;
+export const frontendUrlProd = import.meta.env.VITE_frontendUrlProd;
+export const frontendUrlProdCode = import.meta.env.VITE_frontendUrlProd + "/code";
+export const frontendUrlDev = import.meta.env.VITE_frontendUrlDev;
 
-//for development use uncomment the following line
-
-// export const baseUrl = "http://localhost:1337/";
-
-export const frontendUrlProd = "https://tapshare.xyz";
-export const frontendUrlProdCode = "https://tapshare.xyz/code";
-
-export const frontendUrlDev = "http://127.0.0.1:5173";
+// console.log("baseUrl", baseUrl);
+// console.log("frontendUrlProd", frontendUrlProd);
+// console.log("frontendUrlProdCode", frontendUrlProdCode);
+// console.log("frontendUrlDev", frontendUrlDev);
