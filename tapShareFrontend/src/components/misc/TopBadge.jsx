00c8f70514dac5e05385ad53bb8b10925ed61ee3
NGR-NP <ngr112001@gmail.com>
2024-03-17 21:09:13 +0545

    Improved User Experience in rescheduling expire time

--- tapShareFrontend/src/components/misc/TopBadge.jsx
+++ tapShareFrontend/src/components/misc/TopBadge.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import {
+  CircularProgress,
   FormControl,
   FormHelperText,
   MenuItem,
@@ -20,15 +21,25 @@ export default function TopBadge() {
   const { id } = useParams();
   const [selectedIndex, setSelectedIndex] = useState(1);
   const [value, setValue] = useState(24);
-
+  const [isChanged, setIsChanged] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
 
   useEffect(() => {
     validateInput();
   }, [selectedIndex, value]);
 
+  useEffect(() => {
+    // get index and value form  local storeage and setit on state
+    const index = Number(localStorage.getItem("exp-index"));
+    const val = localStorage.getItem("exp-value");
+    if (index) setSelectedIndex(index);
+    if (val) setValue(val);
+  }, []);
+
   const handleMenuItemClick = (idx) => {
     setSelectedIndex(idx);
+    setIsChanged(true);
   };
 
   const handleInputChange = (e) => {
@@ -37,6 +48,7 @@ export default function TopBadge() {
       setErrorMsg("Only 2 digits are allowed");
     } else {
       setValue(val);
+      setIsChanged(true);
     }
   };
 
@@ -46,30 +58,50 @@ export default function TopBadge() {
 
     if (value < 1) {
       setErrorMsg(`Minimum expires ${selectedOption.label} is 1`);
+      setLoading(false);
     } else if (value > max) {
       setErrorMsg(`Maximum expires ${selectedOption.label} is ${max}`);
+      setLoading(false);
     } else {
       setErrorMsg("");
+
+      if (isChanged) {
+        setLoading(true);
+        const handler = setTimeout(() => {
+          callAPi();
+        }, 550);
+        return () => {
+          clearTimeout(handler);
+        };
+      }
     }
   };
+
   const callAPi = async () => {
+    const response = await axios.get("https://api64.ipify.org?format=json");
+    const ipAddress = response.data.ip;
     const data = {
       time: value,
       type: timeOptions[selectedIndex].value,
+      ip: ipAddress,
     };
-    const res = await axios.post(`${baseUrl}api/v1/update-expires-time`, data);
-    console.log(res, data);
-  };
-  useEffect(() => {
-    if (!errorMsg) {
-      const handler = setTimeout(() => {
-        callAPi();
-      }, 350);
-      return () => {
-        clearTimeout(handler);
-      };
+    const userId = localStorage.getItem("userId");
+    try {
+      const res = await axios.post(
+        `${baseUrl}api/v1/update-expires-time/${userId}`,
+        data
+      );
+      console.log(res, data);
+    } catch (error) {
+      setErrorMsg(error?.response?.data?.message);
+    } finally {
+      setLoading(false);
     }
-  }, [value, selectedIndex]);
+
+    localStorage.setItem("exp-type", data.type);
+    localStorage.setItem("exp-index", selectedIndex);
+    localStorage.setItem("exp-value", data.time);
+  };
 
   return (
     <div className="flex justify-between bg-gray-50 py-4 rounded-md px-3 border-l-green-500 border-l-8">
@@ -92,12 +124,13 @@ export default function TopBadge() {
             <TextField
               id="standard-select-currency"
               select
-              defaultValue="hr"
+              defaultValue={localStorage.getItem("exp-type") || "hr"}
               variant="standard"
             >
               {timeOptions.map((option, idx) => (
                 <MenuItem
                   key={option.value}
+                  selected={idx === selectedIndex}
                   onClick={() => handleMenuItemClick(idx)}
                   value={option.value}
                 >
@@ -105,6 +138,7 @@ export default function TopBadge() {
                 </MenuItem>
               ))}
             </TextField>
+            {loading && <CircularProgress size={20} />}
           </div>
           {errorMsg && (
             <FormHelperText id="my-helper-text" error={!!errorMsg}>
