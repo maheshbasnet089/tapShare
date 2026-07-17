8580e888893801c391461187e9888c840887fd11
Manish Basnet <70196462+maheshbasnet089@users.noreply.github.com>
2026-07-17 09:02:34 +0000

    increase expiration

--- tapShareBackend/app.js
+++ tapShareBackend/app.js
@@ -18,7 +18,7 @@ const corsOptions = {
     "http://localhost:5173",
   ],
   // origin: "http://127.0.0.1:5173",
-  // credentialss: true, //access-control-allows-credentials:true
+  // credentialsss: true, //access-control-allows-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
